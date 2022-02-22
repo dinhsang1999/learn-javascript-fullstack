84815ad8e0cf710bb7a96eb019bf9c471c9cb0cb
Sang <hoangsang2020@gmail.com>
2022-02-22 19:28:59 +0700

    built-in command

---
+++ bien-comments-built_in/built-in.js
@@ -0,0 +1,14 @@
+console.log('Day la thong bao')
+console.warn('Day la canh bao')
+console.error('Day la loi~')
+
+confirm('Xac nhan ban du tuoi')
+prompt('Xac nhan ban du tuoi')
+
+setTimeout(function () {
+    alert('Thong bao')
+}, 1000)
+
+setInterval(function () {
+    console.log('Day la log' + Math.random())
+}, 1000)
---
+++ bien-comments-built_in/commnent.js
@@ -0,0 +1,6 @@
+// Khai bao bien
+var fullName = 'Dinh Hoang Sang'
+var age = 26
+alert(fullName) 
+alert(age)
+/** */
